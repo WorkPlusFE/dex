70b725ca941736e94ad5dc06a079258bdb7c19db
hejianxian <531601727@qq.com>
2020-08-01 17:34:10 +0800

    chore: dex@1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# dex
+# dex 
+
+[![npm version](https://badge.fury.io/js/%40w6s%2Fdex.svg)](https://badge.fury.io/js/%40w6s%2Fdex)
+
 🆒 Diff different commits of git and export incremental packages!
+
+## Install
+
+```bash
+yarn global add @W6s/dex
+# or
+npm install -g @W6s/dex
+```
+
+## Usage
+
+```bash
+Usage: dex [options] <new-version> <old-version> [zip-name]
+
+Diff different commits of git and export incremental packages
+
+Options:
+  -v, --version  output the version number
+  -h, --help     display help for command
+```
+
+## License
+
+MIT
---
+++ bin/dex.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const shell = require('shelljs');
+const path = require('path');
+const { format } = require('date-fns');
+const pkg = require('../package.json');
+const program = new commander.Command();
+
+let currentProjectPkg = {
+  name: 'update',
+};
+try {
+  currentProjectPkg = require(path.join(process.cwd(), '/package.json'));
+} catch (error) {
+  // nothing
+}
+const defaultZipName = `${currentProjectPkg.name}-${format(Date.now(), 'yyyyMMdd')}.zip`;
+
+program.exitOverride((err) => {
+  if (err.code === 'commander.missingArgument') {
+    console.log('');
+    program.outputHelp();
+  }
+  process.exit(err.exitCode);
+});
+
+program
+  .version(pkg.version, '-v, --version')
+  .arguments('<new-version> <old-version> [zip-name]')
+  .description('Diff different commits of git and export incremental packages')
+  .action((newVersion, oldVersion, name) => {
+    if (typeof name === 'undefined') {
+      name = defaultZipName;
+    }
+    if (name.indexOf('.zip') < 0) {
+      name += '.zip';
+    }
+    console.log('');
+    shell.exec(`git diff ${newVersion} ${oldVersion} --name-only | xargs zip ${name}`);
+    console.log(`\n  🆒 Done! your zip file -> ${name}`);
+  });
+
+program.parse(process.argv);
